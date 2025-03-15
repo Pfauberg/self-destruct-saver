ad2b0448ee4a3d034a6363104af3fe6e8aebc2c4
Oleksii Pavlenko <o.pavlenko.email@gmail.com>
2025-03-15 11:13:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import configparser
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+api_id = int(config["telegram"]["api_id"])
+api_hash = config["telegram"]["api_hash"]
+
+bot = Client('self_destruct_saver', api_id=api_id, api_hash=api_hash)
+
+
+@bot.on_message(filters.private & (filters.photo | filters.video))
+async def save_media(client: Client, message: Message):
+    file_path = await message.download()
+
+    if message.photo and message.photo.ttl_seconds:
+        await client.send_photo("me", file_path)
+    elif message.video and message.video.ttl_seconds:
+        await client.send_video("me", file_path)
+
+    os.remove(file_path)
+
+
+bot.run()
